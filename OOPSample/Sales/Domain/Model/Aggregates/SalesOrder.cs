0ae1c3cb1e98034040b6ddbd64ae69b39d2eb9ac
MarceloRenteriaUPC <149633136+MarceloRenteriaUPC@users.noreply.github.com>
2024-03-25 20:28:50 -0500

    feat: created relation between SalesOrder and SalesOrderItem.

--- OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -5,4 +5,11 @@ public class SalesOrder(int id, int customerId)
     public int Id { get; } = id;
     public int customerId { get; } = customerId;
     
+    private readonly List<SalesOrderItem> _items = [];
+    
+    public void AddItem(int productId, int quantity, double unitPrice)
+    {
+        _items.Add(new SalesOrderItem(Id, productId, quantity, unitPrice));
+    }
+    
 }
